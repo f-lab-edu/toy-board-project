f6a82ef715a937b3ee403a75921e8093c9f0c417
choiseon <com1368@naver.com>
2023-04-01 12:02:36 +0900

    first commit

---
+++ src/main/java/com/flab/toyboardproject/ToyBoardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.flab.toyboardproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyBoardProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyBoardProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flab/toyboardproject/ToyBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.toyboardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyBoardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
