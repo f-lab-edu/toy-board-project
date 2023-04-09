79c828e622870ea7654b0d313619ad6684d0a885
choiseon <com1368@naver.com>
2023-04-09 17:06:20 +0900

    fix : MemberService 구현체 클래스에서 인터페이스 의존으로 변경

--- src/main/java/com/flab/toyboardproject/application/MemberService.java
+++ src/main/java/com/flab/toyboardproject/application/MemberService.java
@@ -2,27 +2,11 @@ package com.flab.toyboardproject.application;
 
 import com.flab.toyboardproject.dto.request.MemberSaveRequest;
 import com.flab.toyboardproject.dto.response.MemberInfoResponse;
-import com.flab.toyboardproject.mapper.MemberMapper;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Service
-public class MemberService {
+public interface MemberService {
+    List<MemberInfoResponse> getMemberList();
 
-    private final MemberMapper memberMapper;
-
-    public MemberService(MemberMapper memberMapper) {
-        this.memberMapper = memberMapper;
-    }
-
-    public List<MemberInfoResponse> getMemberList() {
-        return memberMapper.getMemberList();
-    }
-
-    @Transactional
-    public void saveMember(MemberSaveRequest memberSaveRequest) {
-        memberMapper.saveMember(memberSaveRequest.toEntity());
-    }
+    void saveMember(MemberSaveRequest memberSaveRequest);
 }
---
+++ src/main/java/com/flab/toyboardproject/application/MemberServiceImpl.java
@@ -0,0 +1,28 @@
+package com.flab.toyboardproject.application;
+
+import com.flab.toyboardproject.dto.request.MemberSaveRequest;
+import com.flab.toyboardproject.dto.response.MemberInfoResponse;
+import com.flab.toyboardproject.mapper.MemberMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberMapper memberMapper;
+
+    public MemberServiceImpl(MemberMapper memberMapper) {
+        this.memberMapper = memberMapper;
+    }
+
+    @Override
+    public List<MemberInfoResponse> getMemberList() {
+        return memberMapper.getMemberList();
+    }
+
+    @Override
+    public void saveMember(MemberSaveRequest memberSaveRequest) {
+        memberMapper.saveMember(memberSaveRequest.toEntity());
+    }
+}
